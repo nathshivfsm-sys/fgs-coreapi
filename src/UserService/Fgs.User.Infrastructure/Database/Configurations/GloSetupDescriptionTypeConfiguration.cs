86cc7b738d7263462f7969d35a47650b77f41604
Pralhad <User1@VM1-WIN11>
2026-05-19 09:05:01 -0700

    Fixed run time error

--- src/UserService/Fgs.User.Infrastructure/Database/Configurations/GloSetupDescriptionTypeConfiguration.cs
+++ src/UserService/Fgs.User.Infrastructure/Database/Configurations/GloSetupDescriptionTypeConfiguration.cs
@@ -1,7 +1,6 @@
 using Fgs.User.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Fgs.User.Infrastructure.Database.Configurations;
 
@@ -11,11 +10,14 @@ internal class GloSetupDescriptionTypeConfiguration : IEntityTypeConfiguration<G
     {
         entity.ToTable("GloSetupDescriptionType");
         entity.HasKey(e => e.Id);
-        entity.Property(e => e.Id).UseIdentityByDefaultColumn();
-        entity.HasIndex(e => e.Code).IsUnique();
+        entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
+        entity.HasAlternateKey(e => e.Code).HasName("UQ_GloSetupDescriptionType_Code");
         entity.Property(e => e.Code).HasMaxLength(100);
         entity.Property(e => e.Name).HasMaxLength(200);
-        entity.Property(e => e.CreatedOn).HasColumnType("timestamptz");
+        entity.Property(e => e.IsActive).HasDefaultValue(true);
+        entity.Property(e => e.CreatedOn)
+            .HasColumnType("timestamptz")
+            .HasDefaultValueSql("timezone('utc', now())");
         entity.Property(e => e.UpdatedOn).HasColumnType("timestamptz");
     }
 }
